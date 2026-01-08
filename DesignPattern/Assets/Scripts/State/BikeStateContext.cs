d95e0d655f167885d42c18a526fde8c6e717a6ba
Yeon <includeashe@gmail.com>
2026-01-09 08:20:11 +0900

    Feat: 상태패턴으로 캐릭터 상태 관리하기
     - State Pattern 실습 (~p. 52)

---
+++ DesignPattern/Assets/Scripts/State/BikeStateContext.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BikeStateContext : MonoBehaviour
+{
+    public IBikeState CurrentState
+    {
+        get; set;
+    }
+
+    private readonly BikeController _bikeController;
+
+    public BikeStateContext(BikeController bikeController)
+    {
+        _bikeController = bikeController;
+    }
+
+    public void Transition()
+    {
+        CurrentState.Handle(_bikeController);
+    }
+
+    public void Transition(IBikeState state)
+    {
+        CurrentState = state;
+        CurrentState.Handle(_bikeController);
+    }
+}
